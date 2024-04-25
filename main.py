bd0f23ab68ebe46690fc4cfa6f82c1107850c719
HafizhMA <muhammadazhr29@gmail.com>
2024-04-25 20:40:35 +0700

    connect and convert csv to json

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
-from flask import Flask
+from flask import Flask, jsonify
+from flask_cors import CORS
+import pandas as pd
 
 app = Flask(__name__)
+cors = CORS(app, origins='*')
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route("/get-data")
+def get_data():
+    df = pd.read_csv('./dataset/eminatest.csv', nrows=10)
+    # Ubah orientasi menjadi 'records' untuk mendapatkan objek JSON tanpa indeks
+    return df.to_json(orient='records')
+
+if __name__ == "__main__":
+    app.run(debug=True)
