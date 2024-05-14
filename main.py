ec2f73835e63274deb33593517bcd9b67cc1302f
HafizhMA <muhammadazhr29@gmail.com>
2024-05-14 16:12:28 +0700

    labeling testing data menggunakan rumus dan training data

--- main.py
+++ main.py
@@ -2,11 +2,7 @@ from flask import Flask, jsonify
 from collections import Counter
 from flask_cors import CORS
 import pandas as pd
-from textblob import TextBlob
-import nltk
-from nltk.stem import PorterStemmer
-from nltk.tokenize import word_tokenize
-import preprocessor as p
+import numpy as np
 from dotenv import load_dotenv
 import os
 from function.text_cleaner import clear_twitter_text
@@ -15,8 +11,8 @@ from function.stopwords import stopword
 from function.stemming import stemming
 from function.translate import convert_eng
 from function.categorize_trainingdata import categorize_text
-from function.obj_converter import call_palestina_obj, call_palestinacleaned_obj, call_trainingdata_obj
-from models.model import db, User, Palestina, PalestinaCleaned, PalestinaTrainingData
+from function.obj_converter import call_palestina_obj, call_palestinacleaned_obj, call_trainingdata_obj, call_labeleddata_obj
+from models.model import db, User, Palestina, PalestinaCleaned, PalestinaTrainingData, LabeledPalestinaData
 
 load_dotenv()
 app = Flask(__name__)
@@ -30,6 +26,13 @@ db.init_app(app)
 def home():
     return 'connect ke flask berhasil'
 
+@app.route("/sql-to-df")
+def sqltodf():
+    data = call_trainingdata_obj()
+    df = pd.DataFrame(data)
+    json_data = df.to_json(orient='records')
+    # Return JSON response
+    return json_data
 
 # Create and insert table
 @app.route("/create-palestina-table")
@@ -80,6 +83,22 @@ def get_kategori():
     
     # Ambil kolom yang berisi kategori
     kategori_column = [row['categories'] for row in data]
+    
+    # Hitung frekuensi setiap kategori
+    kategori_counter = Counter(kategori_column)
+    
+    # Ubah hasil ke format yang bisa dijadikan JSON
+    kategori_json = [{"kategori": kategori, "jumlah": jumlah} for kategori, jumlah in kategori_counter.items()]
+    # Membuat dictionary yang berisi data asli dan kategori_json
+    total = {"data": data, "kategori": kategori_json}
+    
+    return jsonify(total)
+
+@app.route("/get-labeled-testdata")
+def get_labeled_testdata():
+    data = call_labeleddata_obj()
+    # Ambil kolom yang berisi kategori
+    kategori_column = [row['category'] for row in data]
     
     # Hitung frekuensi setiap kategori
     kategori_counter = Counter(kategori_column)
@@ -208,54 +227,81 @@ def traindata_categorized():
     db.session.commit()
     return "Table training data Palestina berhasil dibuat dan diisi dengan data."
 
+
 # labeling
-@app.route("/labeling-textblob")
-def labelingTextblob():
-    # jangan lupa download nltk, uncomment code dibawah
-    # nltk.download('punkt')
-    df = pd.read_csv('./dataset/eminacleantranslate.csv')
-    df = df.drop_duplicates(subset=["full_text"])
-    df = df.dropna(subset=["full_text"])
-    data_tweet = list(df['tweet_english'])
+@app.route("/labeling-testingdata")
+def labeling_testdata():
+    db.create_all()
 
-    polaritas = 0
-    status = []
-    total_suka = total_tidaksuka = total_netral = total = 0
+    # Load data
+    train_data = call_trainingdata_obj()
+    test_data = call_palestinacleaned_obj()
 
-    for i, tweet in enumerate(data_tweet):
-        analysis = TextBlob(tweet)
-        polaritas += analysis.polarity
+    # Convert train_data and test_data to DataFrames
+    train_df = pd.DataFrame(train_data)
+    test_df = pd.DataFrame(test_data)
 
-        if analysis.sentiment.polarity > 0.0:
-            total_suka += 1
-            status.append('Suka')
-        elif analysis.sentiment.polarity == 0.0:
-            total_netral += 1
-            status.append('Netral')
-        else:
-            total_tidaksuka += 1
-            status.append('Tidak_suka')
+    # Menghitung jumlah kategori dan kata-kata unik dalam data pelatihan
+    categories = train_df['categories'].unique()
+    V = len(train_df['full_text'].str.split().explode().unique())  # Vocab size
+    W = len(categories)  # Jumlah kategori
 
-        total += 1
+    # Menghitung probabilitas prior untuk setiap kategori P(Vj)
+    prior_probs = {}
+    for category in categories:
+        prior_probs[category] = len(train_df[train_df['categories'] == category]) / len(train_df)
 
-    result = [{
-        'suka': total_suka,
-        'netral': total_netral,
-        'tidaksuka': total_tidaksuka,
-        'totaldata': total
-    }]
+    # Membuat fungsi untuk menghitung probabilitas kondisional P(xi|Vj)
+    def conditional_prob(word, category, train_data):
+        word_count_in_category = train_data[train_data['categories'] == category]['full_text'].str.split().apply(lambda x: x.count(word)).sum()
+        total_words_in_category = train_data[train_data['categories'] == category]['full_text'].str.split().apply(len).sum()
+        return (word_count_in_category + 1) / (total_words_in_category + V)
 
-    df['klasifikasi_textblob'] = status
+    # Prediksi kategori untuk setiap dokumen di data pengujian
+    predictions = []
+    for index, row in test_df.iterrows():
+        max_prob = -1
+        pred_category = None
+        for category in categories:
+            prob = prior_probs[category]
+            for word in row['full_text'].split():
+                prob *= conditional_prob(word, category, train_df)
+            if prob > max_prob:
+                max_prob = prob
+                pred_category = category
+        predictions.append(pred_category)
 
-    df_json = df.to_json(orient='records')
+    # Menambahkan kolom prediksi ke data pengujian
+    test_df['predicted_category'] = predictions
 
-    # Menggabungkan DataFrame JSON dan result ke dalam satu dictionary
-    response_data = {
-        'df': df_json,
-        'result': result
-    }
+    # Save labeled data into the database
+    for i, data in enumerate(test_data):
+        new_entry = LabeledPalestinaData(
+            conversation_id_str=data['conversation_id_str'],
+            created_at=data['created_at'],
+            favorite_count=data['favorite_count'],
+            full_text=data['full_text'],
+            id_str=data['id_str'],
+            image_url=data['image_url'],
+            in_reply_to_screen_name=data['in_reply_to_screen_name'],
+            lang=data['lang'],
+            location=data['location'],
+            quote_count=data['quote_count'],
+            reply_count=data['reply_count'],
+            retweet_count=data['retweet_count'],
+            tweet_url=data['tweet_url'],
+            user_id_str=data['user_id_str'],
+            username=data['username'],
+            category=predictions[i]
+        )
+        db.session.add(new_entry)
 
-    return (response_data)
+    db.session.commit()
+    return "The labeled Palestine data table has been successfully created and populated."
+
+
+
+
 
 
 
--- models/model.py
+++ models/model.py
@@ -76,3 +76,26 @@ class PalestinaTrainingData(db.Model):
 
     def __repr__(self):
         return '<PalestinaTrainingData %r>' % self.full_text
+    
+class LabeledPalestinaData(db.Model):
+    __tablename__ = 'labeledpalestinadata'
+    id = db.Column(db.Integer, primary_key=True)
+    conversation_id_str = db.Column(db.String(50))
+    created_at = db.Column(db.String(100))
+    favorite_count = db.Column(db.Integer)
+    full_text = db.Column(db.String(500))
+    id_str = db.Column(db.String(50))
+    image_url = db.Column(db.String(200))
+    in_reply_to_screen_name = db.Column(db.String(100))
+    lang = db.Column(db.String(10))
+    location = db.Column(db.String(100))
+    quote_count = db.Column(db.Integer)
+    reply_count = db.Column(db.Integer)
+    retweet_count = db.Column(db.Integer)
+    tweet_url = db.Column(db.String(200))
+    user_id_str = db.Column(db.String(50))
+    username = db.Column(db.String(100))
+    category = db.Column(db.String(100))
+
+    def __repr__(self):
+        return '<labeledpalestinadata %r>' % self.full_text
