5ae6e719934fcdf6ab11870a208583f300771d50
HafizhMA <muhammadazhr29@gmail.com>
2024-05-20 16:05:23 +0700

    fitur login dan register

--- models/model.py
+++ models/model.py
@@ -5,7 +5,8 @@ db = SQLAlchemy()
 class User(db.Model):
     __tablename__ = 'user' 
     id = db.Column(db.Integer, primary_key=True)
-    nama = db.Column(db.String(100), nullable=False)
+    nama = db.Column(db.String(50), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
 
     def __repr__(self):
         return '<User %r>' % self.nama
